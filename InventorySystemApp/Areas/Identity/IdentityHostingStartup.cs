87878999662479ea4ac78734d28ac7b4b52f3fb8
Ysmerys <ysmecoy2@gmail.com>
2021-03-18 15:05:23 -0500

    Add project files.

---
+++ InventorySystemApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using InventorySystemApp.Areas.Identity.Data;
+using InventorySystemApp.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(InventorySystemApp.Areas.Identity.IdentityHostingStartup))]
+namespace InventorySystemApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<InventorySystemAppDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("InventorySystemAppDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                    .AddEntityFrameworkStores<InventorySystemAppDbContext>();
+            });
+        }
+    }
+}
---
+++ InventorySystemApp/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventorySystemApp.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello World");
+        }
+    }
+}
